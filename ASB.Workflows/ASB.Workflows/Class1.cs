5fe7f4961728db36fdd7b8c7a65ec1c0e6e4bfba
AMAR <AMAR@AMAR>
2021-03-12 21:11:42 +0530

    Solutions Created

---
+++ ASB.Workflows/ASB.Workflows/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASB.Workflows
+{
+    public class Class1
+    {
+    }
+}
